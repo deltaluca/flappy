550f3912fa4586ff5d96e770e169d2cf0d5a2415
Luca Deltodesco <luca@deltaluca.me.uk>
2011-10-12 00:08:16 +0100

    add hGetMessage, hPutMessage to server.hs for reading null terminated strings and writing null terminated strings to socket. works properly with the flash haxe client, though not with the c++ haxe client for some reason.

--- test-haxe-socket/server.hs
+++ test-haxe-socket/server.hs
@@ -10,9 +10,23 @@ listen :: IO ()
 listen = do
   socket <- listenOn (PortNumber 4571)
   (handle, hostName, portNumber) <- accept socket
-  putStrLn hostName
-  string <- hGetContents handle
+  hSetBuffering handle NoBuffering
+
+  hPutMessage handle "y15:hi from server!"
+  string <- hGetMessage handle
   putStrLn string
+  hPutMessage handle "y16:bye from server!"
   
-  
+hGetMessage :: Handle -> IO String
+hGetMessage handle = do
+  c <- hGetChar handle
+  if c == '\0'
+    then return ""
+    else do l <- hGetMessage handle
+            return (c:l)
+
+hPutMessage :: Handle -> String -> IO ()
+hPutMessage handle msg = do
+	hPutStr handle msg
+	hPutChar handle '\0'
   
