6df56e81d3dffc31acd54ca555e070ce93281654
Luca Deltodesco <luca@deltaluca.me.uk>
2011-10-12 16:18:07 +0100

    bloopy loopy doopy

--- test-haxe-socket/Main.hx
+++ test-haxe-socket/Main.hx
@@ -2,21 +2,28 @@ package;
 
 import Socket;
 
+#if flash
+@:bitmap("map_c.gif") class Map extends flash.display.BitmapData {}
+#elseif cpp
+import nme.Assets;
+#end
+
 class Main extends flash.display.Sprite {
-	static function main() {
-		#if flash
-			flash.Lib.current.addChild(new Main());
-		#elseif cpp
-			nme.Lib.create(function() { nme.Lib.current.addChild(new Main()); },
-				400,300,60,0xffffff,
-				nme.Lib.HARDWARE | nme.Lib.VSYNC
-			);
-		#end
+	public static function main() {
+		flash.Lib.current.addChild(new Main());
 	}
 
 	var sock:Socket;
 	function new() {
 		super();
+		addEventListener(flash.events.Event.ADDED_TO_STAGE, init);
+	}
+	function init(_) {
+		#if flash
+		addChild(new flash.display.Bitmap(new Map(800,600)));
+		#elseif cpp
+		addChild(new flash.display.Bitmap(Assets.getBitmapData("assets/map_c.gif")));
+		#end
 
 		sock = new Socket();
 		sock.onReceive = function(dat:Dynamic) {
