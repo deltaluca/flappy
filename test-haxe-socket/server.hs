11328f47ad2b616f435d7327b0e0e8d3dccf23ee
Luca Deltodesco <luca@deltaluca.me.uk>
2011-10-12 07:55:22 +0100

    add haxe serialisation of strings and integers

--- test-haxe-socket/server.hs
+++ test-haxe-socket/server.hs
@@ -13,7 +13,7 @@ listen = do
   hSetBuffering handle NoBuffering
   hSetBinaryMode handle True
 
-  hPutMessage handle "y15:hi from server!"
+  hPutData handle (HxString "hi from server!")
 
   msg <- hGetData handle
   putStrLn $ show msg
@@ -24,7 +24,7 @@ listen = do
   msg <- hGetData handle
   putStrLn $ show msg
 
-  hPutMessage handle "y16:bye from server!"
+  hPutData handle (HxInt 10)
   
 hGetMessage :: Handle -> IO String
 hGetMessage handle = do
@@ -34,9 +34,9 @@ hGetMessage handle = do
     else do l <- hGetMessage handle
             return (c:l)
 
-hPutMessage :: Handle -> String -> IO ()
-hPutMessage handle msg = do
-	hPutStr handle msg
+hPutData :: Handle -> HaxeType -> IO ()
+hPutData handle dat = do
+	hPutStr handle (hx_serialise dat)
 	hPutChar handle '\0'
  
 -----------------------
@@ -44,17 +44,23 @@ hPutMessage handle msg = do
 hGetData :: Handle -> IO HaxeType
 hGetData handle = do
 	string <- hGetMessage handle
-	return (fst $ haxeType string)
+	return (fst $ hx_deserialise string)
 
 -----------------------
 
 data HaxeType = HxString String | HxError String | HxInt Int
 
-haxeType :: String -> (HaxeType, String)
-haxeType (t:rest)
+hx_deserialise :: String -> (HaxeType, String)
+hx_deserialise (t:rest)
   | t == 'y' = hx_decode_str rest
   | t == 'z' = (HxInt 0, rest)
   | t == 'i' = hx_decode_int rest
+
+hx_serialise :: HaxeType -> String
+hx_serialise (HxString str) = ('y' : (show $ length url)) ++ (':' : url)
+  where url = url_encode str
+hx_serialise (HxInt 0) = "z"
+hx_serialise (HxInt x) = 'i' : (show x)
 
 instance Show HaxeType where
   show (HxString str) = "HxString \"" ++ str ++ "\""
@@ -81,6 +87,15 @@ url_decode ('%':'2':'2':rest) = '"' : url_decode rest
 url_decode ('%':'2':'3':rest) = '#' : url_decode rest
 url_decode ('%':'2':'4':rest) = '$' : url_decode rest
 url_decode (x:rest) = x : url_decode rest
+
+url_encode :: String -> String
+url_encode "" = ""
+url_encode (' ':rest) = "%20" ++ url_encode rest
+url_encode ('!':rest) = "%21" ++ url_encode rest
+url_encode ('"':rest) = "%22" ++ url_encode rest
+url_encode ('#':rest) = "%23" ++ url_encode rest
+url_encode ('$':rest) = "%24" ++ url_encode rest
+url_encode (x:rest) = x : url_encode rest
 
 istr :: String -> (Int,String)
 istr = head . reads
