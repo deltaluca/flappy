b7421a6ce91d6ae0e9d33b36bc1197d3bc999afd
Luca Deltodesco <luca@deltaluca.me.uk>
2011-10-12 01:14:00 +0100

    add start of haXe deserialisation in haskell. Deserializing strings (handling a few url encoded chars), and integers

--- test-haxe-socket/server.hs
+++ test-haxe-socket/server.hs
@@ -2,7 +2,7 @@ module Main where
 
 import System.IO
 import Network
-
+import Monad
 
 main = listen
 
@@ -14,8 +14,16 @@ listen = do
   hSetBinaryMode handle True
 
   hPutMessage handle "y15:hi from server!"
-  string <- hGetMessage handle
-  putStrLn string
+
+  msg <- hGetData handle
+  putStrLn $ show msg
+
+  msg <- hGetData handle
+  putStrLn $ show msg
+
+  msg <- hGetData handle
+  putStrLn $ show msg
+
   hPutMessage handle "y16:bye from server!"
   
 hGetMessage :: Handle -> IO String
@@ -30,4 +38,56 @@ hPutMessage :: Handle -> String -> IO ()
 hPutMessage handle msg = do
 	hPutStr handle msg
 	hPutChar handle '\0'
-  
+ 
+-----------------------
+
+hGetData :: Handle -> IO HaxeType
+hGetData handle = do
+	string <- hGetMessage handle
+	return (fst $ haxeType string)
+
+-----------------------
+
+data HaxeType = HxString String | HxError String | HxInt Int
+
+haxeType :: String -> (HaxeType, String)
+haxeType (t:rest)
+  | t == 'y' = hx_decode_str rest
+  | t == 'z' = (HxInt 0, rest)
+  | t == 'i' = hx_decode_int rest
+
+instance Show HaxeType where
+  show (HxString str) = "HxString \"" ++ str ++ "\""
+  show (HxError str) = "HxError \"" ++ str ++ "\""
+  show (HxInt ival) = "HxInt " ++ (show ival)
+
+-----------------------
+
+hx_decode_str :: String -> (HaxeType, String)
+hx_decode_str str = (HxString outstr, reststr)
+  where (size,(_:rest)) = istr str
+        outstr = url_decode $ take size rest
+        reststr = drop size rest
+
+hx_decode_int :: String -> (HaxeType, String)
+hx_decode_int str = (HxInt ival, rest)
+  where (ival,rest) = istr str
+
+url_decode :: String -> String
+url_decode "" = ""
+url_decode ('%':'2':'0':rest) = ' ' : url_decode rest
+url_decode ('%':'2':'1':rest) = '!' : url_decode rest
+url_decode ('%':'2':'2':rest) = '"' : url_decode rest
+url_decode ('%':'2':'3':rest) = '#' : url_decode rest
+url_decode ('%':'2':'4':rest) = '$' : url_decode rest
+url_decode (x:rest) = x : url_decode rest
+
+istr :: String -> (Int,String)
+istr = head . reads
+
+split :: String -> Char -> [String]
+split "" delim = [""]
+split (c:cs) delim
+  | c == delim = "" : rest
+  | otherwise  = (c : head rest) : tail rest
+  where rest = split cs delim
