87ad05544d4706bb600fb52d0afbc83d6fbb6dac
Luca Deltodesco <luca@deltaluca.me.uk>
2011-10-22 23:05:45 +0100

    rework AST to make parsing more sensible

--- User_Client/daide/Language.hx
+++ User_Client/daide/Language.hx
@@ -2,168 +2,43 @@ package daide;
 
 import daide.Tokens;
 import daide.HLlr;
-/*
-using Tokens.TokenUtils;
 
-class MessageUtils {
-	static public function inflate(msg:Array<Token>,?ind=0): {outind:Int, msg:Message} {
-		if(ind >= msg.length) return {outind:ind,msg:null};
-		
-		function accept(tok:Token) {
-			if(Type.enumEq(tok,msg[ind])) {
-				ind++;
-				return true;
-			}else
-				return false;
-		}
-		
-		function expect(tok:Token) {
-			if(Type.enumEq(tok,msg[ind])) {
-				ind++;
-				return true;
-			}
-			throw "Error: Unexpected token, expected "+Std.string(tok)+" found "+Std.string(msg[ind])+" instead";
-			return false;
-		}
-
-		function getString() {
-			if(ind>=msg.length)
-				throw "Error: Expected string, found null instead";
-			var ret = "";
-			while(ind<msg.length) {
-				switch(msg[ind]) {
-					case tText(char):
-						ret += char;
-						ind++;
-					default:
-						if(ret=="")
-							throw "Error: Expected string, found "+Std.string(msg[ind])+" instead";
-						break;
-				}
-			}
-			return ret;
-		}
-
-		function getInt() {
-			if(ind>=msg.length) 
-				throw "Error: Expected integer, found null instead";
-
-			var cur = msg[ind];
-			switch(cur) {
-				case tInteger(val):
-					ind++;
-					return val;
-				default:
-					throw "Error: Expected integer, found "+Std.string(cur)+" instead";
-					return -1;
-			}
-		}
-
-		var ret:Message = null;
-
-		if(accept(tCommand(coName))) {
-			expect(tLeftParen);
-			var name = getString();
-			expect(tRightParen);
-			expect(tLeftParen);
-			var version = getString();
-			expect(tRightParen);
-			ret = mObserver(obsName(name,version));
-		}else if(accept(tCommand(coObserver))) {
-			ret = mObserver(obsObserver);
-		}else if(accept(tCommand(coIAm))) {
-			expect(tLeftParen);
-			var power = getInt();
-			expect(tRightParen);
-			expect(tLeftParen);
-			var passcode = getInt();
-			expect(tRightParen);
-			ret = mObserver(obsIAm(power,passcode));
-		}else if(accept(tCommand(coAccept))) {
-			if(accept(tLeftParen)) {
-				var mid = inflate(msg,ind);
-				if(mid.msg==null) throw "Error: Expected Daide message in YES ( .. )";
-				ind = mid.outind;
-				expect(tRightParen);
-				ret = mObserver(obsAccept(mid.msg));
-			}else
-				ret = mObserver(obsAccept(null));
-		}else if(accept(tCommand(coReject))) {
-			if(accept(tLeftParen)) {
-				var mid = inflate(msg,ind);
-				if(mid.msg==null) throw "Error: Expected Daide message in REJ ( .. )";
-				ind = mid.outind;
-				expect(tRightParen);
-				ret = mObserver(obsReject(mid.msg));
-			}else
-				ret = mObserver(obsReject(null));
-		}
-
-
-		if(ret==null)
-			throw "Unrecognised token stream :: "+msg.slice(ind).join(" ");
-		return {outind:ind, msg:ret};
-	}
-
-	static public function flatten(msg:Message):Array<Token> {
-		return switch(msg) {
-		case mObserver(obs):
-			switch(obs) {
-			case obsName(name,version):
-				[tCommand(coName),tLeftParen]
-				.concat(name.fromString())
-				.concat([tRightParen,tLeftParen])
-				.concat(version.fromString())
-				.concat([tRightParen]);
-			case obsObserver:
-				[tCommand(coObserver)];
-			case obsIAm(power,passcode):
-				[tCommand(coIAm),
-				 tLeftParen,tInteger(power),tRightParen,
-				 tLeftParen,tInteger(passcode),tRightParen];
-			case obsMap(name):
-				if(name==null)
-					[tCommand(coMap)];
-				else
-					[tCommand(coMap),tLeftParen]
-					.concat(name.fromString())
-					.concat([tRightParen]);
-			case obsAccept(msg):
-				if(msg==null)
-					[tCommand(coAccept)];
-				else
-					[tCommand(coAccept),tLeftParen]
-					.concat(flatten(msg))
-					.concat([tRightParen]);
-			case obsReject(msg):
-				if(msg==null)
-					[tCommand(coReject)];
-				else
-					[tCommand(coReject),tLeftParen]
-					.concat(flatten(msg))
-					.concat([tRightParen]);
-			case obsTimeToDeadline(time):
-				if(time==null)
-					[tCommand(coTimeToDeadline)];
-				else
-					[tCommand(coTimeToDeadline),
-					 tLeftParen,tInteger(time),tRightParen];
-			case obsAdmin(name,msg):
-				[tCommand(coAdmin),tLeftParen]
-				.concat(name.fromString())
-				.concat([tRightParen,tLeftParen])
-				.concat(msg.fromString())
-				.concat([tRightParen]);
-			default:
-				throw "fuck";
-			}
-		default:
-			throw "fuck";
-		}	
-	}	
+enum Message {
+	mName(name:String,version:String);
+	mObserver;
+	mIAm(power:Int,passcode:Int);
+	mMap(name:String);
+	mSaveGame(name:String);
+//	mMapDefinition(...);
+	mAccept(m:Message);
+	mReject(m:Message);
+//	mCurrentLocation(...);
+//	mSupplyOwnership(...);
+	mHistory(turn:Turn);
+	mTimeToDeadline(time:Null<Int>);
+	mAdmin(a:String,b:String);
+//	mBadBracketedSequence(...);
+//	mHuh(...);
+	mHello;
+	mSubmit(turn:Turn,orders:Array<MsgOrder>);
 }
-*/
 
+enum MsgOrder {
+	moHold(unit:UnitWithLoc);
+	moDisband(unit:UnitWithLoc);
+	moBuild(unit:UnitWithLoc);
+	moRemove(unit:UnitWithLoc);
+	moRetreat(unit:UnitWithLoc, loc:Location);
+	moMove(unit:UnitWithLoc, loc:Location);
+	moSupport(unit:UnitWithLoc, unit2:UnitWithLoc, move:Province);
+	moConvoy(unit:UnitWithLoc, unit2:UnitWithLoc, to:Province);
+	moMoveByConvoy(unit:UnitWithLoc, from:Province, via:Array<Province>); 
+}
+
+typedef Turn = { phase : Phase, turn : Int };
+typedef Location = { province : Province, coast : Coast };
+typedef UnitWithLoc = { power : Int, type : UnitType, location : Location };
+/*
 enum ObsMessage {
 	obsName(name:String,version:String);
 	obsObserver;
@@ -223,4 +98,4 @@ enum Message {
 	mObs(msg:ObsMessage);
 	mLvl0(msg:Lvl0Message);
 	mLvl10(msg:Lvl10Message);
-}
+}*/
