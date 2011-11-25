dbea109fcaf6b871183f39a007da7fe8250b7388
Cliff Sun <chs09@ic.ac.uk>
2011-11-25 12:53:22 +0000

    more of holdbot done

--- AI/Diplomacy/AI/Bots/HoldBot/holdbot.hs
+++ AI/Diplomacy/AI/Bots/HoldBot/holdbot.hs
@@ -6,17 +6,26 @@ import Common.Diplomacy.Common.DipMessage
 
 data HoldDecision = HoldDecision [UnitPosition]
 data DisbandDecision = DisbandDecision [UnitPosition]
-data WaiveDecision = WaiveDecision [UnitPosition]
-data WaiveAndDisband = WaiveAndDisband WaiveDecision DisbandDecision -- <- may not work, is ugly, fix it for me Cliff you gay 
--- why do we even need waiveanddisband?
+data WaiveDecision Power = WaiveDecision Power
 
 instance Decision HoldBrain HoldDecision where
   diplomise :: Decision -> [DipMessage] -- <- Maybe not actually DipMessages
-  diplomise (HoldDecision units) = functionThatWritesHoldOrderForEachUnit
-  diplomise (DisbandDecision units) = -- function that sends disband order for units
-  diplomise (WaiveDecision units) = -- function that sends waive build order for units 
+  diplomise (HoldDecision units) = SubmitOrder map holdOrderForUnit units 
+  diplomise (DisbandDecision units) = SubmitOrder map disbandOrderForUnit units
+  diplomise (WaiveDecision power) = SubmitOrder Order OrderBuild Waive power
 
 diplomise Hold = asd
+
+--produces an order to hold the unit
+holdOrderForUnit :: UnitPosition -> Order
+holdOrderForUnit unit = Order OrderMovement Hold unit 
+
+--produces an order to disband the unit
+disbandOrderForUnit :: UnitPosition -> Order
+disbandOrderForUnit unit = Order OrderRetreat Disband unit
+
+getPower :: Power
+getPower = --gets our power, from gameInfo, Slemi still hasn't pushed his changes for that >.<
 
 type HoldBrain = BrainComm HoldDecision ()
 
@@ -24,9 +33,9 @@ main = skelBot holdBrain
 
 holdBrain :: HoldBrain ()
 holdBrain = do
-  curMapState <- askMapState
-  unitPoss <- unitPositions curMapState
-  ownSupplies <- getOwnSupplies $ supplyOwnerships curMapState --need to define getOwnSupplies here
+  curMapState <- asksGameState gameStateMap
+  let unitPoss = unitPositions curMapState
+  let ownSupplies = getOwnSupplies $ supplyOwnerships curMapState --need to define getOwnSupplies here
   decide $ holdUnits unitPoss ownSupplies
 
   {-forever $ do
@@ -41,7 +50,7 @@ holdBrain = do
 
 getOwnSupplies :: [SupplyCentreOwnership] -> [Provinces]
 getOwnSupplies supplies = 
-  [provinces | (power provinces) <- supplies, {-check that power is us-}]
+  [provinces | (power provinces) <- supplies, power = getPower]
 
 holdUnits :: UnitPositions -> [SupplyCentreOwnership] -> Decision
 holdUnits (UnitPositions Spring units) _ =
@@ -77,7 +86,7 @@ retreatUnits unitsAndRetreats =
 disbandOrWaive :: [UnitPosition] ->  [Provinces] -> Decision
 disbandOrWaive units supplyCentres =
   | difference > 0 = DisbandDecision $ take difference units
-  | otherwise      = WaiveDecision units
+  | otherwise      = WaiveDecision getPower
      {- here we need to waive a build on a unit on a province in our home territory 
         which we are still in control of and doesn't have a unit on it currently -}
     -- waive builds on all units? Technically waiving a build means not submitting any sort of build order whatsoever
@@ -90,6 +99,6 @@ disbandOrWaive units supplyCentres =
    else
    we need to waive builds, we never build so we just say no thanks don't want to
    build
+  UPDATE: just waive power does the same thing. BOOM.
 -}
-
 
