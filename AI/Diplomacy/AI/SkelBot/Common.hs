73335dd0c1a101d4886cc1b9e0789b5f427311f0
Ex Falso <0slemi0@gmail.com>
2011-12-19 16:32:14 +0000

    added a few more conv functions

--- AI/Diplomacy/AI/SkelBot/Common.hs
+++ AI/Diplomacy/AI/SkelBot/Common.hs
@@ -1,7 +1,9 @@
 -- |Common module (convenience functions) for all bots
 
 module Diplomacy.AI.SkelBot.Common( getMyPower
+                                  , getSupplies
                                   , getMySupplies
+                                  , getUnits
                                   , getMyUnits
                                   ) where
 
@@ -11,28 +13,34 @@ import Diplomacy.Common.Data
 import Data.Map hiding (map)
 import Control.Monad
 
+-- convention: for every getX there should be a getMyX
+
 getMyPower :: OrderClass o => Brain o h Power
 getMyPower = asksGameInfo gameInfoPower
 
-getMySupplies :: OrderClass o => Brain o h [Province]
-getMySupplies = do
+getSupplies :: OrderClass o => Power -> Brain o h [Province]
+getSupplies power = do
   curMapState <- asksGameState gameStateMap
-  myPower <- getMyPower
   let SupplyCOwnerships supplies = supplyOwnerships curMapState
-  return (supplies ! myPower)
+  return (supplies ! power)
 
-getMyUnits :: OrderClass o => Brain o h [UnitPosition]
-getMyUnits = do
+getMySupplies :: OrderClass o => Brain o h [Province]
+getMySupplies = getSupplies =<< getMyPower
+
+getUnits :: OrderClass o => Power -> Brain o h [UnitPosition]
+getUnits power = do
   (GameState mapState (Turn phase _)) <- askGameState
-  myPower <- getMyPower
   
   case unitPositions mapState of
     UnitPositions units -> do
       if not $ phase `elem` [Spring, Fall, Winter]
         then error $ "Wrong Phase (Got " ++ show phase ++ ", expected [Spring, Fall, Winter]"
-        else return (units ! myPower)
+        else return (units ! power)
       
     UnitPositionsRet units -> do
       if not $ phase `elem` [Summer, Autumn]
         then error $ "Wrong Phase (Got " ++ show phase ++ ", expected [Summer, Autumn]"
-        else return (map fst $ units ! myPower)
+        else return (map fst $ units ! power)
+
+getMyUnits :: OrderClass o => Brain o h [UnitPosition]
+getMyUnits = getUnits =<< getMyPower
