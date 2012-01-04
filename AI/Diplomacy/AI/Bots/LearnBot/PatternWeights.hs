0eafbcaef17e71d091f40e690eca5a0deb70a295
tetigi <luke.tomlin@gmail.com>
2012-01-04 11:35:24 +0000

    Finished inital metrics for patter weights in learnbot

--- AI/Diplomacy/AI/Bots/LearnBot/PatternWeights.hs
+++ AI/Diplomacy/AI/Bots/LearnBot/PatternWeights.hs
@@ -19,13 +19,70 @@ import Database.HDBC.Sqlite3
 
 import qualified Data.Map as Map
 
-doNothing :: ( Functor m, Monad m, OrderClass o, MonadBrain o m, MonadGameKnowledge h m) => m ()
-doNothing = do
-  return ()
+data MOrderType = THold
+                | TMove
+                | TSupportHold
+                | TSupportMove
+                | TConvoy
+                | TMoveConvoy
+                deriving (Eq, Ord, Show)
+
+mOTTmap :: Map.Map MOrderType Int
+mOTTmap = Map.fromList $ zip [THold,TMove,TSupportHold,TSupportMove,TConvoy,TMoveConvoy] [1..] 
+
+mOT2Int :: MOrderType -> Int
+mOT2Int = (mOTTmap Map.!)
+
+bool2Int :: Bool -> Int
+bool2Int b = if b then 1 else 0
+
+-- f : MoveOrder -> Int
+metrics :: (Functor m, Monad m, OrderClass o, MonadBrain o m, MonadGameKnowledge h m) => [(OrderMovement -> m Int)]
+metrics = [(\x -> return . bool2Int =<< targNodeFriendly =<< moveOrderToTargProv x)
+          ,(\x -> return . bool2Int =<< targNodeOccupied =<< moveOrderToTargProv x)
+          ,(\x -> return . bool2Int =<< targNodeIsSupply =<< moveOrderToTargProv x)
+          ,(\x -> targNodeAdjUnits            =<< moveOrderToTargProv x)
+          ,(\x -> return . bool2Int =<< targNodeFriendly =<< moveOrderToOwnProv x)
+          ,(\x -> return . bool2Int =<< targNodeOccupied =<< moveOrderToOwnProv x)
+          ,(\x -> return . bool2Int =<< targNodeIsSupply =<< moveOrderToOwnProv x)
+          ,(\x -> targNodeAdjUnits            =<< moveOrderToOwnProv x)
+          ,(\x -> return . mOT2Int                     =<< moveOrderToType x)]
+
+moveOrderToOwnProv :: (Functor m, Monad m, OrderClass o, MonadBrain o m, MonadGameKnowledge h m) => OrderMovement -> m Province
+moveOrderToOwnProv order = 
+  case order of 
+    Hold u            -> return . provNodeToProv . unitPositionLoc $ u
+    Move u _          -> return . provNodeToProv . unitPositionLoc $ u
+    SupportHold u _   -> return . provNodeToProv . unitPositionLoc $ u  
+    SupportMove u _ _ -> return . provNodeToProv . unitPositionLoc $ u
+    Convoy u _ _      -> return . provNodeToProv . unitPositionLoc $ u
+    MoveConvoy u _ _  -> return . provNodeToProv . unitPositionLoc $ u
+ 
+moveOrderToTargProv :: (Functor m, Monad m, OrderClass o, MonadBrain o m, MonadGameKnowledge h m) => OrderMovement -> m Province
+moveOrderToTargProv order = 
+  case order of 
+    Hold u            -> return . provNodeToProv . unitPositionLoc $ u
+    Move u _          -> return . provNodeToProv . unitPositionLoc $ u
+    SupportHold _ u   -> return . provNodeToProv . unitPositionLoc $ u  
+    SupportMove _ u _ -> return . provNodeToProv . unitPositionLoc $ u
+    Convoy _ u _      -> return . provNodeToProv . unitPositionLoc $ u
+    MoveConvoy u _ _  -> return . provNodeToProv . unitPositionLoc $ u
+  
+
+moveOrderToType :: (Functor m, Monad m, OrderClass o, MonadBrain o m, MonadGameKnowledge h m) => OrderMovement -> m MOrderType
+moveOrderToType order =
+  case order of 
+    Hold _            -> return THold 
+    Move _ _          -> return TMove
+    SupportHold _ _   -> return TSupportHold
+    SupportMove _ _ _ -> return TSupportMove
+    Convoy _ _ _      -> return TConvoy
+    MoveConvoy _ _ _  -> return TMoveConvoy
 
 average :: [Double] -> Double
 average l = (sum l) / ((fromIntegral.length) l)
 
+-- weigh an order using metrics listed in metric, and access database based on function id and return value (an integer)
 weighOrder :: (Functor m, Monad m, OrderClass o, MonadBrain o m, MonadGameKnowledge h m) => OrderMovement -> m Double
 weighOrder = undefined
 
@@ -48,3 +105,26 @@ randWeightedElem elemWeights = do
   if len == 0
     then error "randWeightedElem called with empty list"
     else return $ results !! index
+
+targNodeOccupied :: (OrderClass o, MonadBrain o m, MonadGameKnowledge h m) => Province -> m Bool 
+targNodeOccupied prov = do
+  unitMap <- getProvUnitMap
+  case prov `Map.lookup` unitMap of
+    Nothing -> return False
+    _ -> return True
+
+targNodeFriendly :: (OrderClass o, MonadBrain o m, MonadGameKnowledge h m) => Province -> m Bool
+targNodeFriendly prov = do
+  unitMap <- getProvUnitMap
+  myPower <- getMyPower
+  case prov `Map.lookup` unitMap of
+    Just u -> return (myPower == (unitPositionP u))
+    _ -> return True --Empty province is friendly I guess?
+
+targNodeIsSupply :: (OrderClass o, MonadBrain o m, MonadGameKnowledge h m) => Province -> m Bool 
+targNodeIsSupply prov = do
+  return $ provinceIsSupply prov
+
+targNodeAdjUnits :: (OrderClass o, MonadBrain o m, MonadGameKnowledge h m) => Province -> m Int
+targNodeAdjUnits prov = do
+  return.length =<< getAdjacentUnits prov
