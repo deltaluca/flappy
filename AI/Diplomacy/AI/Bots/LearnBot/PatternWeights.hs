57f8ef9c9df9b2cf3afe264ab3a266e5c78cf66c
tetigi <luke.tomlin@gmail.com>
2012-01-09 16:21:36 +0000

    PatterWeights completely done, just needs tweaking when we can finally test the AI

--- AI/Diplomacy/AI/Bots/LearnBot/PatternWeights.hs
+++ AI/Diplomacy/AI/Bots/LearnBot/PatternWeights.hs
@@ -1,14 +1,6 @@
 {-
- - PATTERN WEIGHTS DATABASE
- - Current version is missing database functionality. I have no real idea how to do this ><
- -
- - Once database functionality is working however, should function for a 1-pattern learning bot. A couple of modifications should enable it to work for 2, and n-pattern bots also. 
- -
- - Currently, I guess database should be a mapping from functions and value
- - to a weight and age
- - (function ID, value) -> (Weight, Age)
- - ie. (Int, Int) -> (Double, Int)
- -}
+ - PATTERN WEIGHTS DATABASE FUNCTIONS
+-}
 
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -32,20 +24,24 @@ import Control.Applicative
 
 import System.Random
 
-import Data.Maybe
 import Data.List
 import Database.HDBC
 import Database.HDBC.Sqlite3
 
 import qualified Data.Map as Map
 
+-- Dummy type kludge
+data Dummy o m = Dummy (m o)
+
 -----------------------------------------------------------------------
 -- Bot-specific variables
 -- Metrics, nPatterns, Database name, Weighting of applyTDiffTurn, Variance of K
 
+-- database name
 _dbname :: String
 _dbname = "test.db"
 
+-- n pattern weights to use
 _npats :: [Int]
 _npats = [1,2]
 
@@ -54,6 +50,8 @@ _npats = [1,2]
 _c :: Double
 _c = 10.0
 
+-- sets the low (starting) and high (ending) values of k, which varies linearly over the 
+-- game period from low to high. k is used as a 'learning temperature'
 _lowK :: Double
 _lowK = 1.0
 _highK :: Double
@@ -63,37 +61,28 @@ _highK = 5.0
 _noOfSCNeededToWin :: Int
 _noOfSCNeededToWin = 18
 
+-- _metrics to use
 -- ordering is important!
-metrics :: (OrderClass o, MonadIO m) => Dummy o m -> [OrderMovement -> LearnBrainT o m Int]
-metrics _ = [(\x -> return . bool2Int =<< targNodeFriendly =<< moveOrderToTargProv x)
+_metrics :: (OrderClass o, MonadIO m) => Dummy o m -> [OrderMovement -> LearnBrainT o m Int]
+_metrics _ = [(\x -> return . bool2Int =<< targNodeFriendly =<< moveOrderToTargProv x)
             ,(\x -> return . bool2Int =<< targNodeOccupied =<< moveOrderToTargProv x)
             ,(\x -> return . bool2Int =<< targNodeIsSupply =<< moveOrderToTargProv x)
-            ,(\x -> targNodeAdjUnits            =<< moveOrderToTargProv x)
-            ,(\x -> targNodeAdjUnits            =<< moveOrderToOwnProv x)
-            ,(\x -> return . mOT2Int                     =<< moveOrderToType x)]
+            ,(\x ->                       targNodeAdjUnits =<< moveOrderToTargProv x)
+            ,(\x ->                       targNodeAdjUnits =<< moveOrderToOwnProv x)
+            ,(\x -> return . mOT2Int                       =<< moveOrderToType x)]
 
 -----------------------------------------------------------------------
-
--- arbitrary numberings for the various types of move orders
-data MOrderType = THold
-                | TMove
-                | TSupportHold
-                | TSupportMove
-                | TConvoy
-                | TMoveConvoy
-                deriving (Eq, Ord, Show)
+-----------------------------------------------------------------------
+-- Pattern specific functions
 
 data Pattern = Pattern {  getMetrics :: [Int]
                        ,  getN :: Int
                        ,  getID :: Int }
 
-instance Eq Pattern where
-  a == b  = (getID a) == (getID b)
-
-applyPattern :: (OrderClass o, MonadIO m) => Pattern -> OrderMovement -> LearnBrainT o m (Int,Int)
+applyPattern :: (OrderClass o, MonadIO m) => Pattern -> OrderMovement -> LearnBrainT o m (Int,Int,Int)
 applyPattern patt order = do
-  mVals <- sequence [(metrics undefined !! (i-1)) order | i <- getMetrics patt]
-  return (getID patt, ncant mVals)
+  mVals <- sequence [(_metrics undefined !! (i-1)) order | i <- getMetrics patt]
+  return (getID patt, ncant mVals, getN patt)
 
 generatePatterns :: (OrderClass o, MonadIO m) => Dummy o m -> [Int] -> [Int -> Pattern]
 generatePatterns d ns = concat [[(Pattern ms n) | ms <- combN n (metricIDs d)] | n <- ns]
@@ -117,6 +106,19 @@ combN n l
 getLists :: Int -> [a] -> [[a]] 
 getLists n l = takeWhile ((>= n).length) $ iterate tail l
 
+-----------------------------------------------------------------------
+-----------------------------------------------------------------------
+-- Metric specific stuff
+
+-- arbitrary numberings for the various types of move orders
+data MOrderType = THold
+                | TMove
+                | TSupportHold
+                | TSupportMove
+                | TConvoy
+                | TMoveConvoy
+                deriving (Eq, Ord, Show)
+
 moveOrderToType :: (OrderClass o, MonadIO m) => OrderMovement -> LearnBrainT o m MOrderType
 moveOrderToType order =
   case order of 
@@ -126,6 +128,14 @@ moveOrderToType order =
     SupportMove _ _ _ -> return TSupportMove
     Convoy _ _ _      -> return TConvoy
     MoveConvoy _ _ _  -> return TMoveConvoy
+
+moveOrderToOwnProv :: (OrderClass o, MonadIO m) => OrderMovement -> LearnBrainT o m Province
+moveOrderToOwnProv = 
+  return . provNodeToProv . unitPositionLoc . getOMSubjectUnit
+ 
+moveOrderToTargProv :: (OrderClass o, MonadIO m) => OrderMovement -> LearnBrainT o m Province
+moveOrderToTargProv =
+    return . provNodeToProv . unitPositionLoc . getOMTargetUnit
 
 mOTTmap :: Map.Map MOrderType Int
 mOTTmap = Map.fromList $ zip [THold,TMove,TSupportHold,TSupportMove,TConvoy,TMoveConvoy] [1..] 
@@ -137,45 +147,35 @@ bool2Int :: Bool -> Int
 bool2Int b = if b then 1 else 0
 
 -----------------------------------------------------------------
--- Pattern generation
-
-
-data Dummy o m = Dummy (m o)
+-----------------------------------------------------------------
+-- Pattern generation functions
 
 metricIDs :: (OrderClass o, MonadIO m) => Dummy o m -> [Int]
-metricIDs d = take (length (metrics d)) [1..]
+metricIDs d = take (length (_metrics d)) [1..]
 
 zipApply :: [a -> b] -> [a] -> [b]
 zipApply fs xs = map (\(f,x) -> f x) $ zip fs xs
 
-patterns :: (OrderClass o, MonadIO m) => Dummy o m -> [Pattern]
-patterns d  = zipApply (generatePatterns d _npats) [1..]
+_patterns :: (OrderClass o, MonadIO m) => Dummy o m -> [Pattern]
+_patterns d  = zipApply (generatePatterns d _npats) [1..]
 
-moveOrderToOwnProv :: (OrderClass o, MonadIO m) => OrderMovement -> LearnBrainT o m Province
-moveOrderToOwnProv = 
-  return . provNodeToProv . unitPositionLoc . getOMSubjectUnit
- 
-moveOrderToTargProv :: (OrderClass o, MonadIO m) => OrderMovement -> LearnBrainT o m Province
-moveOrderToTargProv =
-    return . provNodeToProv . unitPositionLoc . getOMTargetUnit
+--------------------------------------------------------------------
+--------------------------------------------------------------------
+-- Order weighing and relevant database functions
 
 average :: [Double] -> Double
 average l = (sum l) / ((fromIntegral.length) l)
 
---------------------------------------------------------------------
--- order weighing 
--- 
-
---WTF HASKELL WTF I HATE YOU
-
-updatePatternsGetWeightAge :: Connection -> (Int,Int) -> IO (Double,Int)
-updatePatternsGetWeightAge conn (pid, pval) = do 
+-- given a connection, (pattern ID, pattern value, pattern size), pulls weight
+-- from database and returns (weight, age, pattern size)
+updatePatternsGetWeightAge :: Connection -> (Int,Int,Int) -> IO (Double,Int,Int)
+updatePatternsGetWeightAge conn (pid, pval, psize) = do 
   result <- quickQuery' conn "SELECT pid, pval FROM test WHERE pid = ?" [toSql pid] 
-  if (length result == 0) 
+  if (length result == 0) -- if not in database, use default value
     then run conn "INSERT INTO test VALUES (?,?,0.5,1)" [toSql pid, toSql pval]
     else run conn "UPDATE test SET age = (age + 1) WHERE pid = ?" [toSql pid]
   weightsResult <- quickQuery' conn "SELECT weight, age FROM test WHERE pid = ? AND pval = ?" [toSql pid, toSql pval]
-  return $ (\[x,y] -> (fromSql x, fromSql y)::(Double,Int)) $ head weightsResult
+  return $ (\[x,y] -> (fromSql x, fromSql y, psize)::(Double,Int,Int)) $ head weightsResult
 
 sortGT :: (Double, a) -> (Double, a) ->  Ordering
 sortGT (d1,_) (d2,_)
@@ -184,20 +184,19 @@ sortGT (d1,_) (d2,_)
 
 weighOrder :: forall o m. (MonadIO m, OrderClass o) => OrderMovement -> LearnBrainT o m (Double,[(Int,Int)])
 weighOrder order = do
-  keyVals <- sequence [applyPattern p order | p <- patterns (undefined :: Dummy o m)]
- 
-  conn <- liftIO $ connectSqlite3 _dbname--hardcoded for now
-  
-  weightAges <- liftIO (sequence $ map (updatePatternsGetWeightAge conn) keyVals)
+  keyVals <- sequence [applyPattern p order | p <- _patterns (undefined :: Dummy o m)]
+  conn <- liftIO $ connectSqlite3 _dbname
+  weightAgeSizes <- liftIO (sequence $ map (updatePatternsGetWeightAge conn) keyVals)
   
   liftIO $ commit conn
   liftIO $ disconnect conn
  
-  -- weights are linearly biased by their ages 
-  let weights =  map (\(x,y) -> x + (fromIntegral y)) weightAges
-
-  -- need to consider the weighting of the age
-  return $ (average weights,keyVals)
+  -- weights are linearly biased by their ages and pattern size
+  let weights =  map (\(weight,age,patSize) -> (weight*(fromIntegral patSize)) + (fromIntegral age)) weightAgeSizes
+  return $ (average weights, peelR keyVals) --remove patSize because it is no longer required
+    where
+      peelR :: [(a,a,a)] -> [(a,a)]
+      peelR  = (\(x,y,z) -> zip x y) . unzip3 
 
 weighOrderSet :: (MonadIO m, OrderClass o) => [OrderMovement] -> LearnBrainT o m ((Double, [OrderMovement]),[(Int,Int)])
 weighOrderSet orders = do
@@ -205,29 +204,24 @@ weighOrderSet orders = do
   let (weights, keys) = unzip orderKeys
   return ((average weights, orders),concat keys)
 
+-- the all in one "calc weights and do all turn specific pattern stuff" function
 weighOrderSets :: (MonadIO m, OrderClass o) => [[OrderMovement]] -> LearnBrainT o m [(Double, [OrderMovement])]
 weighOrderSets orderSets = do
   hist <- getHistory
+
   weightKeys <- mapM weighOrderSet orderSets
   let (weights, keys) = unzip weightKeys
   stateValue <- getStateValue
+
   putHistory (hist ++ [(stateValue, concat keys)])
   (return . (sortBy sortGT)) weights
 
-randWeightedElem :: (MonadIO m, OrderClass o) => [(Double, [a])] -> LearnBrainT o m [a]
-randWeightedElem elemWeights = do
-  let (weights, results) = unzip elemWeights
-  let sumWeights = sum weights
-  let cumProb = scanl1 (+) $ map (/sumWeights) weights
-  ranDouble <- getRandomR (0.0,1.0)
-  let index = length $ takeWhile (< ranDouble) cumProb
-  let len = length results
-  if len == 0
-    then error "randWeightedElem called with empty list"
-    else return $ results !! index
 
-------------------------------------------------------------------------
--- metrics
+--------------------------------------------------------------------
+--------------------------------------------------------------------
+-- Pre-defined metrics
+
+-- unit specifc metrics ---------
 
 targNodeOccupied :: (MonadIO m, OrderClass o) => Province -> LearnBrainT o m Bool 
 targNodeOccupied prov = do
@@ -252,9 +246,8 @@ targNodeAdjUnits :: (MonadIO m, OrderClass o) => Province -> LearnBrainT o m Int
 targNodeAdjUnits prov = do
   return.length =<< getAdjacentUnits prov
 
-
---metrics for calculating state value
-
+-- metrics for calculating state value of a given turn -------
+--
 getStateValue :: (MonadIO m, OrderClass o) => LearnBrainT o m Double
 getStateValue = do
    getSupplyCentresValue
@@ -265,13 +258,12 @@ getSupplyCentresValue = do
   (sc, nonsc) <- getProvOcc myPower
   return $ (fromIntegral sc)/(fromIntegral _noOfSCNeededToWin)
 
+--------------------------------------------------------------------
+--------------------------------------------------------------------
+-- Temporal Difference Learning and relevant database functions
 
-
------------------------------------------------------------------------
--- temporal learning
-
--- takes the list of ordermovements metrics and the return values that resulted in a successful streak, and applies temporal difference learning over the entire database
-
+-- apply TDL for turn-by-turn patterns (ie. change in state value observed by those patterns)
+-- takes [(stateValue,[Keys])] 
 applyTDiffTurn :: [(Double,[(Int,Int)])] -> IO ()
 applyTDiffTurn turnValKeys = do
   let l = length turnValKeys 
@@ -287,7 +279,6 @@ updateDBTurns l (n,turnKeys) weightFu = do
    commit conn
    disconnect conn
 
-
 updateDBTurns' :: Connection -> Int -> Int -> (Double -> Double -> Double -> Double) -> (Int,Int) -> IO ()
 updateDBTurns' conn n l weightFu (pid,pval) = do
   weight <- (return .fromSql . head . head) =<< quickQuery' conn "SELECT weight FROM test WHERE pid = ? AND pval = ?" [toSql pid, toSql pval]
@@ -295,7 +286,6 @@ updateDBTurns' conn n l weightFu (pid,pval) = do
   run conn "UPDATE test SET weight = weight WHERE pid = ? AND pval = ?" [toSql newWeight, toSql pid, toSql pval]
   return ()
   
-
 evaluateChangeStates :: [Double] -> [(Double -> Double -> Double -> Double)]
 evaluateChangeStates [x] = []
 evaluateChangeStates (x:x':xs) =
@@ -311,6 +301,9 @@ vfromD d w
   | d <= 0  = (d+1)*w
   | d > 0   = d*(1-w)+w
 
+-------------------------------------------------
+
+-- apply TDL for endgame patterns (ie. based on whether pattern led to successful game)
 applyTDiffEnd :: [[(Int,Int)]] -> IO ()
 applyTDiffEnd succTurnKeys = do
   -- keys should be a subset of dbkeys, as new entries should be added as they're not found whilst the game is being played
@@ -342,12 +335,26 @@ updateDB finalVals = do
     commit conn
     disconnect conn
 
--- given the current turn and number of turns, returns a new k to simulate annealing
--- for now just increases linearly as the turns gets closer to the end
--- arbitrary values (from paper), ranges from 1 - 5
+-- generate temperature based on placement in game
 getK :: Int -> Int -> Double
 getK n l = _lowK + ((_highK - _lowK) * ((fromIntegral n)/(fromIntegral l)))
 
 getNextWeight :: Double -> Int -> Double -> Bool -> Double
 getNextWeight prev n k win = (prev*(fromIntegral (n-1)) + k*(fromIntegral v))/(k + fromIntegral (n-1))
   where v = bool2Int win
+
+--------------------------------------------------------------------
+--------------------------------------------------------------------
+-- Externally used functions
+
+randWeightedElem :: (MonadIO m, OrderClass o) => [(Double, [a])] -> LearnBrainT o m [a]
+randWeightedElem elemWeights = do
+  let (weights, results) = unzip elemWeights
+  let sumWeights = sum weights
+  let cumProb = scanl1 (+) $ map (/sumWeights) weights
+  ranDouble <- getRandomR (0.0,1.0)
+  let index = length $ takeWhile (< ranDouble) cumProb
+  let len = length results
+  if len == 0
+    then error "randWeightedElem called with empty list"
+    else return $ results !! index
